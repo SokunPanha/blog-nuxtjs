e2af6937981276d969ca196c3a2bd235ac131e4e
Panha <sokunpanha.panha@gmail.com>
2026-02-05 12:59:38 +0700

    configure nuxt modules, components, CSS, fonts, and i18n

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,51 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/content', '@nuxt/image', '@nuxt/ui']
+  modules: [
+    '@nuxt/content',
+    '@nuxt/image',
+    '@nuxt/ui',
+    '@nuxtjs/i18n',
+    '@nuxt/fonts'
+  ],
+  components: [
+    '~/components/ui',
+    '~/components/ui/layouts',
+    '~/components/ui/common',
+    '~/components/features',
+  ],
+  css: [
+    '~/assets/css/main.css',
+  ],
+  fonts: {
+    families:  [
+      {
+        name: 'Roboto',
+        weights: [400, 500, 700],
+        provider: 'google',
+      },
+      {
+        name: 'Inter',
+        weights: [400, 500, 700],
+        provider: 'google',
+      }
+    ]
+  },
+  i18n:{
+    locales: [
+      {
+        code: 'en',
+        name: 'English',
+        file: 'en.json',
+      },
+      {
+        code: 'kh',
+        name: 'Khmer',
+        file: 'kh.json',
+      },
+    ],
+    defaultLocale: 'en',
+    strategy: 'no_prefix',
+    langDir: 'locales',
+  }
 })
