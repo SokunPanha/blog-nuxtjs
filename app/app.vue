dc12fe796d520bd6ca8b6cd80d9ca51507ba548d
Panha <sokunpanha.panha@gmail.com>
2026-02-12 17:40:38 +0700

    fixed: flicky ui on home page

--- app/app.vue
+++ app/app.vue
@@ -2,23 +2,22 @@
 const NoneAdminLayoutPaht = ["/admin/auth/login"];
 const route = useRoute();
 const layoutName = computed(() => {
-  if (route.path.startsWith('/admin') && !NoneAdminLayoutPaht.includes(route.path)) {
-    return 'admin-layout'
-  } else if (route.path.startsWith('/admin')) {
-    return false
-  } else if (route.path.startsWith('/blog')) {
-    return false
+  if (
+    route.path.startsWith("/admin") &&
+    !NoneAdminLayoutPaht.includes(route.path)
+  ) {
+    return "admin-layout";
+  } else if (route.path.startsWith("/admin")) {
+    return false;
   } else {
-    return 'blog-layout'
+    return "blog-layout";
   }
-})
+});
 </script>
 
 <template>
   <UApp :toaster="{ position: 'top-right' }">
-    <NuxtLayout
-      :name="layoutName"
-    >
+    <NuxtLayout :name="layoutName">
       <div class="relative min-h-screen">
         <NuxtPage />
       </div>
